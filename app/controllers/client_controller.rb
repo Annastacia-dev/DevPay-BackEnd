370caec0507d74befb23e9d49d88bcc23e423832
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-11-09 11:50:42 +0300

    remove update from post clients

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -16,15 +16,6 @@ class ClientController < Sinatra::Base
         if Client.find_by(email: params([:email]))
             status 401
             {message: "Email already exists"}.to_json
-            @client = Client.find_by(email: params([:email]))
-            @client.update(
-                name: params[:name],
-                email: params[:email],
-                phone_number: params[:phone_number],
-                location: params[:location],
-                developer_id: params[:developer_id],
-            )
-            @client.to_json
         else
         @client = Client.create(
             name: params[:name],
