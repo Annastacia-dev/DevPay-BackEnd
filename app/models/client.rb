ad4827cdc8a5a526991fc7350e9e0f624f6b8f5b
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-11-09 11:32:19 +0300

    client & developer validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,5 @@ class Client < ActiveRecord::Base
     belongs_to :developer
     has_many :services
     has_many :invoices, :through => :services
+    validates_presence_of :name, :email, :phone_number, :location, :developer_id
 end
